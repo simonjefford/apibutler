d661554d6cfc474d09046f7c5251c60707dd301d
Simon Jefford <simon.jefford@gmail.com>
2014-02-27 11:50:23 +0000

    fix jshint errors in test backend

--- testbackend/test.js
+++ testbackend/test.js
@@ -3,21 +3,21 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res) {
-  res.send({
-    response: {
-      foo: 42,
-      bar: [1,2,3],
-      baz: { a: "b"}
-    }
-  });
+    res.send({
+        response: {
+            foo: 42,
+            bar: [1,2,3],
+            baz: { a: 'b'}
+        }
+    });
 });
 
 app.get('/foo', function(req, res) {
-  res.send({
-    response: {
-      endpoint: "new"
-    }
-  });
+    res.send({
+        response: {
+            endpoint: 'new'
+        }
+    });
 });
 
 app.listen(3000);
