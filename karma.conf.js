5223ce2ddc870fd4699930caf3bf37b2474c0fdf
Simon Jefford <simon.jefford@gmail.com>
2014-03-17 11:45:04 +0000

    frontend: karma now working properly again
    Karma > 0.10.0 seems to struggle to count qunit tests
    
    Also cleaned up the karma config file a little bit

--- karma.conf.js
+++ karma.conf.js
@@ -1,78 +1,51 @@
 // Karma configuration
-// Generated on Sat Mar 01 2014 10:53:38 GMT+0000 (GMT)
+// Generated on Mon Mar 17 2014 10:55:13 GMT+0000 (GMT)
 
-module.exports = function(karma) {
-    karma.configure({
-
-        // base path, that will be used to resolve files and exclude
+module.exports = function(config) {
+    config.set({
+        // base path that will be used to resolve all patterns (eg. files, exclude)
         basePath: '',
 
-
         // frameworks to use
+        // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
         frameworks: ['qunit'],
-
 
         // list of files / patterns to load in the browser
         files: [
-            'public/js/libs/jquery-2.0.0.js',
-            'public/js/libs/handlebars-1.1.2.js',
-            'public/js/libs/ember-1.3.1.js',
-            'public/js/libs/ember-easyform.js',
-            'public/js/libs/ic-ajax.js',
-            'public/js/app.js',
+            'frontend/bower_components/jquery/jquery.js',
+            'frontend/bower_components/handlebars/handlebars.runtime.js',
+            'frontend/bower_components/ember/ember.js',
+            'frontend/bower_components/ic-ajax/dist/globals/main.js',
+            'frontend/bower_components/ember-easyForm/index.js',
+            'frontend/bower_components/bootstrap-sass/js/collapse.js',
+            'frontend/js/app.js',
             'tests/*.js'
         ],
 
-
-        // list of files to exclude
-        exclude: [
-
-        ],
-
-
         // test results reporter to use
-        // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
+        // possible values: 'dots', 'progress'
+        // available reporters: https://npmjs.org/browse/keyword/karma-reporter
         reporters: ['progress'],
-
 
         // web server port
         port: 9876,
 
-
-        // cli runner port
-        runnerPort: 9100,
-
-
         // enable / disable colors in the output (reporters and logs)
         colors: true,
 
-
         // level of logging
-        // possible values: karma.LOG_DISABLE || karma.LOG_ERROR || karma.LOG_WARN || karma.LOG_INFO || karma.LOG_DEBUG
-        logLevel: karma.LOG_DISABLE,
-
+        // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+        logLevel: config.LOG_INFO,
 
         // enable / disable watching file and executing tests whenever any file changes
         autoWatch: true,
 
-
-        // Start these browsers, currently available:
-        // - Chrome
-        // - ChromeCanary
-        // - Firefox
-        // - Opera
-        // - Safari (only Mac)
-        // - PhantomJS
-        // - IE (only Windows)
+        // start these browsers
+        // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
         browsers: ['PhantomJS'],
 
-
-        // If browser does not capture in given timeout [ms], kill it
-        captureTimeout: 60000,
-
-
         // Continuous Integration mode
-        // if true, it capture browsers, run tests and exit
+        // if true, Karma captures browsers, runs the tests and exits
         singleRun: false
     });
 };
