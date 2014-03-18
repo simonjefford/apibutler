fc4f41c8ddf030b40eae43e288e95217f002eb55
Simon Jefford <simon.jefford@gmail.com>
2014-03-18 11:37:50 +0000

    frontend: bigger spinner

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -57,9 +57,20 @@ App.NavbarLinkComponent = Ember.Component.extend({
 });
 
 App.LoadingSpinnerComponent = Ember.Component.extend({
+    spinner: undefined,
+    lines: 13,
+    length: 20,
+    width: 10,
+    radius: 30,
+
     showSpinner: function() {
         var target = this.get('element');
-        this.spinner = new Spinner();
+        this.spinner = new Spinner({
+            lines: this.get('lines'),
+            length: this.get('length'),
+            width: this.get('width'),
+            radius: this.get('radius')
+        });
         this.spinner.spin(target);
     }.on('didInsertElement'),
 
