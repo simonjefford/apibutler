29eb3854e082e14ac46b862c1e00cf734b801dec
Simon Jefford <simon.jefford@gmail.com>
2014-04-03 08:17:09 +0100

    metadata: extract redis code into new file

--- metadata/api.go
+++ metadata/api.go
@@ -1,13 +1,5 @@
 package metadata
 
-import (
-	"encoding/json"
-	"fmt"
-	"log"
-
-	"github.com/garyburd/redigo/redis"
-)
-
 type Api struct {
 	Fragment  string `json:"fragment"`
 	Limit     int    `json:"limit"`
@@ -22,77 +14,4 @@ type ApiStorage interface {
 	AddApi(a *Api)
 	Apis() ([]*Api, error)
 	Forget(path string)
-}
-
-type redisApiStore struct {
-	rdb redis.Conn
-}
-
-func redisConfigKeyForApi(p string) string {
-	return fmt.Sprintf("%s:config", p)
-}
-
-func (r *redisApiStore) AddApi(a *Api) {
-	ret, err := r.rdb.Do("RPUSH", "knownPaths", a.Fragment)
-	a.ID = ret.(int64)
-
-	enc, _ := json.Marshal(a)
-
-	ret, err = r.rdb.Do("SET", redisConfigKeyForApi(a.Fragment), string(enc))
-	fmt.Println(err, ret)
-}
-
-func (r *redisApiStore) Forget(path string) {
-}
-
-func GetApiStore() (ApiStorage, error) {
-	conn, err := redis.Dial("tcp", ":6379")
-	if err != nil {
-		return nil, err
-	}
-
-	return &redisApiStore{conn}, nil
-}
-
-func (r *redisApiStore) Apis() ([]*Api, error) {
-	n, err := redis.Int(r.rdb.Do("LLEN", "knownPaths"))
-
-	if err != nil {
-		return nil, err
-	}
-
-	log.Println(n, "known paths")
-
-	retApis := make([]*Api, 0, n)
-
-	if n == 0 {
-		return retApis, nil
-	}
-
-	paths, _ := redis.Strings(r.rdb.Do("LRANGE", "knownPaths", 0, n))
-
-	if err != nil {
-		return nil, err
-	}
-
-	for idx := range paths {
-		r.rdb.Send("GET", redisConfigKeyForApi(paths[idx]))
-		if err != nil {
-			return nil, err
-		}
-	}
-
-	r.rdb.Flush()
-
-	for _ = range paths {
-		config, _ := redis.String(r.rdb.Receive())
-		if err != nil {
-			return nil, err
-		}
-		var a Api
-		json.Unmarshal([]byte(config), &a)
-		retApis = append(retApis, &a)
-	}
-
-	return retApis, nil
 }
