fa1963b35d548ba7154a9f100d0aa3f564694e66
Simon Jefford <simon.jefford@gmail.com>
2014-04-07 09:06:07 +0100

    frontend: extract data-table component

--- frontend/js/controllers/apis_controller.js
+++ frontend/js/controllers/apis_controller.js
@@ -1,5 +1,7 @@
 /* global App */
 
 App.ApisController = Ember.ArrayController.extend({
-    apisToShow: Ember.computed.filterBy('content', 'isNew', false)
+    apisToShow: Ember.computed.filterBy('content', 'isNew', false),
+
+    headers: ['Path', 'App'],
 });
