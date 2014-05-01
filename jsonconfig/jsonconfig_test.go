7f62d8a7e63ff54f554450a548bf96dfdb9a8759
Simon Jefford <simon.jefford@gmail.com>
2014-05-01 08:43:36 +0100

    jsonconfig: test validation

--- jsonconfig/jsonconfig_test.go
+++ jsonconfig/jsonconfig_test.go
@@ -17,3 +17,26 @@ func Test_SimpleCreate(t *testing.T) {
 		t.Errorf("Expected 1, got %d", val)
 	}
 }
+
+func Test_Validate(t *testing.T) {
+	json := `{"intval": 1, "stringval": "foo", "objval": {"foo": 42}}`
+	o, err := Create(strings.NewReader(json))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	o.RequiredInt("intval")
+	o.RequiredString("stringval")
+	o.RequiredObject("objval")
+	o.RequiredObject("missingobj")
+
+	err = o.Validate()
+
+	if err == nil {
+		t.Fatal("No error was thrown")
+	}
+
+	if err.Error() != "Missing required config key \"missingobj\" (object)" {
+		t.Errorf("Unexpected error %v", err)
+	}
+}
