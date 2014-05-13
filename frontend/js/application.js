4f52c0c387d086e51747c28e14effde08db0af60
Simon Jefford <simon.jefford@gmail.com>
2014-05-13 09:21:27 +0100

    frontend: make store globally available for debugging purposes
    must stick this behind some kind of 'development environment' flag

--- frontend/js/application.js
+++ frontend/js/application.js
@@ -1,3 +1,13 @@
 window.App = Ember.Application.create({
     LOG_TRANSITIONS: true
 });
+
+
+Ember.Application.initializer({
+    name: 'store-debugger',
+    after: 'store',
+
+    initialize: function(container) {
+        window.Store = container.lookup('store:main');
+    }
+});
