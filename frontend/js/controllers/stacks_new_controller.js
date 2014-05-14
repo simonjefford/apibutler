cd32a7e171ec57448c4197ef9a121fa10f240a85
Simon Jefford <simon.jefford@gmail.com>
2014-05-14 19:49:51 +0100

    frontend: basic adding and removing of mw from a new stack

--- frontend/js/controllers/stacks_new_controller.js
+++ frontend/js/controllers/stacks_new_controller.js
@@ -3,16 +3,45 @@ App.StacksNewController = Ember.ObjectController.extend({
 
     middlewareQuery: '',
 
+    unselectedMiddlewares: function() {
+        return this.get('availableMiddlewares').filter(function(item) {
+            var selected = item.get('selected');
+            return Ember.isEmpty(selected) || !selected;
+        });
+    }.property('availableMiddlewares',
+               'availableMiddlewares.isFulfilled',
+               'availableMiddlewares.@each.selected'),
+
     filteredMiddlewares: function() {
-        console.log('filter');
         if (Ember.isEmpty(this.get('middlewareQuery'))) {
-            return this.get('availableMiddlewares');
+            return this.get('unselectedMiddlewares');
         }
 
         var filterExp = new RegExp(this.get('middlewareQuery'), 'i');
 
-        return this.get('availableMiddlewares').filter(function(item) {
+        return this.get('unselectedMiddlewares').filter(function(item) {
             return filterExp.test(item.get('friendlyName'));
         });
-    }.property('middlewareQuery', 'availableMiddlewares')
+    }.property('middlewareQuery', 'unselectedMiddlewares'),
+
+    middlewareRemaining: Ember.computed.bool('unselectedMiddlewares.length'),
+
+    actions: {
+        addToStack: function(mw) {
+            mw.set('selected', true);
+            this.set('middlewareQuery', '');
+            var middlewares = this.get('middlewares');
+            middlewares.pushObject(Ember.Object.create({
+                name: mw.get('name'),
+                underlying: mw,
+                config: {},
+                parent: middlewares
+            }));
+        },
+
+        removeFromStack: function(mw) {
+            mw.parent.removeObject(mw);
+            mw.get('underlying').toggleProperty('selected');
+        }
+    }
 });
