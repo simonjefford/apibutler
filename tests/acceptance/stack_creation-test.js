fe66736f1405351552aca1292b7d540350f5e08c
Simon Jefford <simon.jefford@gmail.com>
2014-05-22 10:03:24 +0100

    frontend: stack creation tests - a beginning

---
+++ tests/acceptance/stack_creation-test.js
@@ -0,0 +1,22 @@
+import startApp from 'apibutler/tests/helpers/start-app';
+
+var App;
+
+module('Acceptance Tests - Stack Creation', {
+	setup: function() {
+		App = startApp();
+	},
+	teardown: function() {
+		Ember.run(App, 'destroy');
+	}
+});
+
+test('Stack creation pane', function() {
+	visit('/stacks/new').then(function() {
+		var stackPaneTitle = find('.new_stack .title');
+		equal(stackPaneTitle.text(), 'New stack', 'Stack creation pane title');
+
+		var stacks = find('.new_stack .stack_items');
+		equal(stacks.length, 0, 'Stack creation pane is empty');
+	});
+});
