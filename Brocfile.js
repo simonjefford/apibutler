249e492bb5d76e27b1cdf495a08f96339025c820
Simon Jefford <simon.jefford@gmail.com>
2014-06-10 12:02:55 +0100

    upgrade to ember-cli 0.0.33

--- Brocfile.js
+++ Brocfile.js
@@ -7,8 +7,6 @@ var env = process.env.EMBER_ENV || 'development';
 var app = new EmberApp({
     name: require('./package.json').name,
 
-    wrapInEval: env !== 'production',
-
     minifyCSS: {
         enabled: true,
         options: {}
@@ -27,15 +25,28 @@ app.import('vendor/jquery-ui/ui/jquery-ui.js');
 app.import('vendor/ember-easyForm/index.js');
 app.import('vendor/bootstrap-sass-official/vendor/assets/javascripts/bootstrap/collapse.js');
 app.import('vendor/bootstrap-sass-official/vendor/assets/javascripts/bootstrap/modal.js');
+// Use `app.import` to add additional libraries to the generated
+// output files.
+//
+// If you need to use different assets in different
+// environments, specify an object as the first parameter. That
+// object's keys should be the environment name and the values
+// should be the asset to use in that environment.
+//
+// If the library that you are including contains AMD or ES6
+// modules that you would like to import into your application
+// please specify an object with the list of modules as keys
+// along with the exports of each module as its value.
+
 app.import({
     development: 'vendor/ember-data/ember-data.js',
     production:  'vendor/ember-data/ember-data.prod.js'
+}, {
+    'ember-data': [
+        'default'
+    ]
 });
 
-// If the library that you are including contains AMD or ES6 modules that
-// you would like to import into your application please specify an
-// object with the list of modules as keys along with the exports of each
-// module as its value.
 app.import('vendor/ic-ajax/dist/named-amd/main.js', {
     'ic-ajax': [
         'default',
