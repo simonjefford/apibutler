e4ecca0613dd341df46deaa44fa7fc760b3b275b
Simon Jefford <simon.jefford@gmail.com>
2014-06-16 14:54:23 +0100

    extract api.new logic into a mixin

--- frontend/controllers/apis/new.js
+++ frontend/controllers/apis/new.js
@@ -1,23 +1,4 @@
-var ApisNewController = Ember.ObjectController.extend({
-    appsReady: Ember.computed.alias('apps.isFulfilled'),
-
-    stacksReady: Ember.computed.alias('stacks.isFulfilled'),
-
-    saveEnabled: Ember.computed.and('appsReady', 'valid', 'stacksReady'),
-
-    saveDisabled: Ember.computed.not('saveEnabled'),
-
-    saveSucceeded: false,
-
-    showSuccessAlert: Ember.computed.and('saveSucceeded', 'isClean'),
-
-    actions: {
-        submit: function() {
-            if (this.get('valid')) {
-                this.send('save', this.get('content'));
-            }
-        }
-    }
+import ApiMixin from 'apibutler/mixins/api';
+export default Ember.ObjectController.extend(ApiMixin, {
+    title: 'New Api'
 });
-
-export default ApisNewController;
---
+++ frontend/mixins/api.js
@@ -0,0 +1,21 @@
+export default Ember.Mixin.create({
+    appsReady: Ember.computed.alias('apps.isFulfilled'),
+
+    stacksReady: Ember.computed.alias('stacks.isFulfilled'),
+
+    saveEnabled: Ember.computed.and('appsReady', 'valid', 'stacksReady'),
+
+    saveDisabled: Ember.computed.not('saveEnabled'),
+
+    saveSucceeded: false,
+
+    showSuccessAlert: Ember.computed.and('saveSucceeded', 'isClean'),
+
+    actions: {
+        submit: function() {
+            if (this.get('valid')) {
+                this.send('save', this.get('content'));
+            }
+        }
+    }
+});
