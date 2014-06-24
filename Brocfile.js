4b7b77ddead975a60477410d2b83cd13f7a1fc8f
Simon Jefford <simon.jefford@gmail.com>
2014-06-24 07:46:06 +0100

    upgrade to ember-cli 0.0.35

--- Brocfile.js
+++ Brocfile.js
@@ -1,4 +1,4 @@
-/* global require, module, process  */
+/* global require, module */
 
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
@@ -18,14 +18,6 @@ var app = new EmberApp({
 });
 
 // Use this to add additional libraries to the generated output files.
-app.import('vendor/spin/index.js');
-app.import('vendor/ember-easyForm/index.js');
-app.import('vendor/bootstrap-sass-official/vendor/assets/javascripts/bootstrap/collapse.js');
-app.import('vendor/bootstrap-sass-official/vendor/assets/javascripts/bootstrap/modal.js');
-app.import('vendor/html5sortable/jquery.sortable.js');
-// Use `app.import` to add additional libraries to the generated
-// output files.
-//
 // If you need to use different assets in different
 // environments, specify an object as the first parameter. That
 // object's keys should be the environment name and the values
@@ -36,24 +28,10 @@ app.import('vendor/html5sortable/jquery.sortable.js');
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
 
-app.import({
-    development: 'vendor/ember-data/ember-data.js',
-    production:  'vendor/ember-data/ember-data.prod.js'
-}, {
-    'ember-data': [
-        'default'
-    ]
-});
-
-app.import('vendor/ic-ajax/dist/named-amd/main.js', {
-    'ic-ajax': [
-        'default',
-        'defineFixture',
-        'lookupFixture',
-        'raw',
-        'request',
-    ]
-});
-
+app.import('vendor/spin/index.js');
+app.import('vendor/ember-easyForm/index.js');
+app.import('vendor/bootstrap-sass-official/vendor/assets/javascripts/bootstrap/collapse.js');
+app.import('vendor/bootstrap-sass-official/vendor/assets/javascripts/bootstrap/modal.js');
+app.import('vendor/html5sortable/jquery.sortable.js');
 
 module.exports = app.toTree();
